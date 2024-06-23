91f63f061c6f342951d42513fdcc6f3a5f146329
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-06-23 20:36:15 +0100

    add favicon to my app

--- Project/settings.py
+++ Project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'statics/'
+
+# Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [
+    BASE_DIR / "App/statics",
+]
+
+# Absolute filesystem path to the directory that will hold static files
+STATIC_ROOT = BASE_DIR / 'statics'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
